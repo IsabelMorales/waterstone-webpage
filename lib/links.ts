6706df42ff3072eac6b0b4df7ddb32ce18bb05fb
Isabel Morales <mariaisabelm1997@gmail.com>
2026-08-11 13:54:04 -0400

    changed url appfolio

--- lib/links.ts
+++ lib/links.ts
@@ -1,5 +1,5 @@
 export const TENANT_LOGIN_URL =
-  'https://account.appfolio.com/realms/foliospace/protocol/openid-connect/auth?client_id=account-picker&redirect_uri=https%3A%2F%2Faccount.appfolio.com%2Frealms%2Ffoliospace%2Fappfolio%2Faccount-picker%2Flogin-redirect%3Fpath%3D%2F&state=0%2Fb60f35ab-321b-dcbb-ba29-d9f020637b7c&response_type=code&scope=openid';
+  'https://account.appfolio.com/realms/foliospace/protocol/openid-connect/auth?activation_state&branding&client_id=client-9918ee7ecfdf2e8b3f40b5443d665d8cee138a07&portfolio_uuid=9854c7fe-1858-11ef-8b4b-02814898e98b&redirect_uri=https%3A%2F%2Fwaterstonegroup.appfolio.com%2Fconnect%2Fusers%2Foauth%2Fcallback&response_type=code&scope=openid&session_timed_out=false&state=%2Fconnect%2F';
 
 export const APPFOLIO_LISTINGS_HOST = 'waterstonegroup.appfolio.com';
 export const APPFOLIO_LISTINGS_URL = `https://${APPFOLIO_LISTINGS_HOST}/listings`;
